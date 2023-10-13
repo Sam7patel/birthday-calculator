830c5d6153c0532837a062121372c5dbb92375f0
Sam7patel <samarthpatel734@gmail.com>
2023-10-13 13:08:47 +0530

    added new things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [birthdate, setBirthdate] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculateAge = () => {
+    const currentDate = new Date();
+    const inputDate = new Date(birthdate);
+
+    if (isNaN(inputDate.getTime())) {
+      setResult('Invalid date');
+    } else if (inputDate > currentDate) {
+      setResult('Birthdate cannot be in the future');
+    } else {
+      const age = Math.floor((currentDate - inputDate) / (1000 * 60 * 60 * 24 * 365));
+      setResult(`You are ${age} years old.`);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Birthday Calculator</h1>
+      <div>
+        <label>Enter your birthdate: </label>
+        <input
+          type="date"
+          value={birthdate}
+          onChange={(e) => setBirthdate(e.target.value)}
+        />
+      </div>
+      <button onClick={calculateAge}>Calculate Age</button>
+      <div className="result">{result}</div>
     </div>
   );
 }
